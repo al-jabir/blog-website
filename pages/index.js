4af2c972f316794b925199c73ad9cf326329c478
al-jabir <aljabir098@gmail.com>
2022-11-08 21:47:15 +0600

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className='text-center font-bold text-lime-300 mt-20 font-cursive font-serif hover:text-black duration-1000'>
+      <h1 className='text-center font-bold text-lime-300 mt-20 font-cursive font-serif hover:text-black duration-1000 text-3xl'>
         I will be learn blog project with nextJS
       </h1>
     </div>
