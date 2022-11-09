1335b6e2b5657b9a43b4320bcb22e82df47e89c5
al-jabir <aljabir098@gmail.com>
2022-11-09 14:13:45 +0600

    author done components

---
+++ components/_child/Author.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const author = () => {
+  return (
+    <div className='author flex py-5'>
+      <Image
+        src={'/images/author/author1.jpg'}
+        alt='author'
+        width={50}
+        height={50}
+      />
+      <div className='flex flex-col justify-center px-4'>
+        <Link href={'/'}>
+          <a className='text-md font-bold text-gray-800 hover:text-gray-600'>
+            Fyling High
+          </a>
+        </Link>
+        <span className='text-sm font-normal'>
+          CEO and Director at softTech
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default author;
