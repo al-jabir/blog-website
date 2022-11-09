45f6859b6aed0281e87708c414e00b545e60f90e
al-jabir <aljabir098@gmail.com>
2022-11-09 20:01:00 +0600

    added swiper done

--- components/Section.jsx
+++ components/Section.jsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import Author from './_child/Author';
 
 const Section = () => {
+  SwiperCore.use([Autoplay]);
   return (
     <section
       className='py-16'
@@ -13,7 +17,18 @@ const Section = () => {
       }}>
       <div className='container mx-auto md:px-20'>
         <h1 className='font-bold text-4xl pb-12 text-center'>Trending</h1>
-        {Slide()}
+
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+          }}>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+          <SwiperSlide>{Slide()}</SwiperSlide>
+        </Swiper>
       </div>
     </section>
   );
