2da78d293f227a441267b1fb92a3acb05db0c6db
al-jabir <aljabir098@gmail.com>
2022-11-09 20:25:26 +0600

    section 2 done

--- components/Section2.js
+++ components/Section2.js
@@ -6,7 +6,14 @@ const Section2 = () => {
   return (
     <section className='container mx-auto md:px-20 py-10'>
       <h1 className='font-bold text-4xl py-12 text-center'>Latest Post</h1>
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-14'></div>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-14'>
+        {Post()}
+        {Post()}
+        {Post()}
+        {Post()}
+        {Post()}
+        {Post()}
+      </div>
     </section>
   );
 };
